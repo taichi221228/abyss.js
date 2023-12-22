a7302276767066c95c4a478d672c1401a665972a
taichi221228 <taichi221228@icloud.com>
2023-12-22 17:41:13 +0900

    Add ID generator script in ES6
    A new script `id-maker` has been added using ES6 syntax. The script utilizes generator function to create a series of IDs which are then logged to the console.

---
+++ scripts/generator/id-maker/es6.ts
@@ -0,0 +1,12 @@
+function* idMaker() {
+    for (let i = 0; ; i++) {
+        yield i;
+    }
+}
+
+const gen = idMaker();
+
+/**
+ * @return {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+ */
+[...Array(10)].forEach(() => console.log(gen.next().value));
