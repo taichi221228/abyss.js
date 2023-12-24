888ed258574136ecaabcec5d9777e3ee38098c1b
taichi221228 <taichi221228@icloud.com>
2023-12-24 13:55:15 +0900

    Add closure-based generator function
    The commit introduces `generatorWithClosure`, a generator function using closure in the "generator.ts" file inside the scripts/generator directory. This function allows for the creation and manipulation of sequences with a state that can be updated and checked against a predicate to control the sequence generation process.

---
+++ scripts/generator/generator.ts
@@ -0,0 +1,35 @@
+export const generatorWithClosure = <T, TReturn, TNext>(
+  state: TNext,
+  update: (state: TNext) => { value: T; next: TNext },
+  predicate: (state: TNext) => boolean,
+): Generator<T, TReturn, TNext> => {
+  const $state = {
+    value: state,
+  };
+
+  const $update = () => {
+    const { value, next } = update($state.value);
+    $state.value = next;
+    return { value };
+  };
+
+  return ({
+    next(..._args): IteratorResult<T, TReturn> {
+      if (predicate(state)) {
+        return { value: undefined as TReturn, done: true };
+      } else {
+        const { value } = $update();
+        return { value, done: false };
+      }
+    },
+    return(value: TReturn): IteratorResult<T, TReturn> {
+      return { value, done: true };
+    },
+    throw(e: Error): IteratorResult<T, TReturn> {
+      throw e;
+    },
+    [Symbol.iterator]: function () {
+      return this;
+    },
+  });
+};
